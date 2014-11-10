4c35b7554a0ea7a6050f0583085373d2e3e16748
anastasa27 <anastasiakonecky@gmail.com>
2014-11-10 13:41:00 -0500

    modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :stacks
   has_many :cards, through: :stacks
-  # validates :username, :password, presence: true
+  validates :username, :password, presence: true
   has_secure_password
 end
